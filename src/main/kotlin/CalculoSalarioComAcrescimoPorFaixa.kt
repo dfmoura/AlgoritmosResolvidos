0037015f39edf66c4efd412f921546ca2f98aa66
dfmoura <diogomou@gmail.com>
2022-10-09 18:37:49 -0300

    CalculoSalarioComAcrescimoPorFaixa.kt SomaDeDoisNumeroInteiros.kt MediaA

--- src/main/kotlin/CalculoSalarioComAcrescimoPorFaixa.kt
+++ src/main/kotlin/CalculoSalarioComAcrescimoPorFaixa.kt
@@ -7,20 +7,25 @@
  * NÃO: Aplicar acréscimo de 1,05
  * Imprimir novo salário atual
  */
+import java.util.Scanner
 
 fun main() {
 //    Entrada de dados
 
-    val salarioAtual = 1000
+    val salarioAtual = Scanner(System.`in`)
+    print(message = "Inorme o Salário Atual: ")
+    val integer:Int = salarioAtual.nextInt()
+    val a:Double
+    val b:Double
+
 
 //    Processamento
-    if (salarioAtual <= 800.00) {
-        (salarioAtual * 1.25).toInt()
-        println(message = "Salário atual (Acréscimo de 1.25): R$ $salarioAtual")
+    if (integer <= 800.00) {
+        a = (integer * 1.25)
+        println(message = "Salário atual (Acréscimo de 1.25): R$ $a")
     } else {
-        (salarioAtual * 1.05).toInt()
-
-//    Saída de dados
-        println(message = "Salário atual (Acréscimo de 1.05): R$ $salarioAtual")
+        b = (integer * 1.05)
+        println(message = "Salário atual (Acréscimo de 1.05): R$ $b")
     }
+//    Saída de dados }
 }
